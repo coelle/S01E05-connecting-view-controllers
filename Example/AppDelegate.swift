1123d7453557743e3983256dd1618464ae35355d
Niels Cölle <niels@coelle-online.com>
2018-06-01 08:01:45 +0200

    Migrated to Swift 4.1

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -39,18 +39,19 @@ class EpisodesViewController: UITableViewController {
     let episodes = [Episode(title: "Episode One"), Episode(title: "Episode Two"), Episode(title: "Episode Three")]
     var didSelect: (Episode) -> () = { _ in }
     var didTapProfile: () -> () = {}
-    
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+
+
+	override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let episode = episodes[indexPath.row]
         didSelect(episode)
     }
-    
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return episodes.count
     }
     
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath)
+	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
         let episode = episodes[indexPath.row]
         cell.textLabel?.text = episode.title
         return cell
@@ -66,22 +67,22 @@ class EpisodesViewController: UITableViewController {
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
         let nc = window?.rootViewController as! UINavigationController
         let episodesVC = nc.viewControllers[0] as! EpisodesViewController
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         episodesVC.didSelect = { episode in
-            let detailVC = storyboard.instantiateViewControllerWithIdentifier("Detail") as! DetailViewController
+			let detailVC = storyboard.instantiateViewController(withIdentifier: "Detail") as! DetailViewController
             detailVC.episode = episode
             nc.pushViewController(detailVC, animated: true)
         }
         episodesVC.didTapProfile = {
-            let profileNC = storyboard.instantiateViewControllerWithIdentifier("Profile") as! UINavigationController
+			let profileNC = storyboard.instantiateViewController(withIdentifier: "Profile") as! UINavigationController
             let profileVC = profileNC.viewControllers[0] as! ProfileViewController
             profileVC.didTapClose = {
-                nc.dismissViewControllerAnimated(true, completion: nil)
+				nc.dismiss(animated: true, completion: nil)
             }
-            nc.presentViewController(profileNC, animated: true, completion: nil)
+			nc.present(profileNC, animated: true, completion: nil)
         }
         return true
     }
