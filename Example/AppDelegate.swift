0ef4f6e9e5ccf5fba0ad60a47fb005b6e63346b9
Niels at home <ncoelle@gmx.de>
2018-08-12 17:39:34 +0200

    Reformatted

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -1,8 +1,6 @@
 //
-//  AppDelegate.swift
 //  Example
 //
-//  Created by Chris Eidhof on 17/05/16.
 //  Copyright © 2016 objc.io. All rights reserved.
 //
 
@@ -10,47 +8,47 @@ import UIKit
 
 
 struct Episode {
-    var title: String
+	var title: String
 }
 
 
 class ProfileViewController: UIViewController {
-    var person: String = ""
+	var person: String = ""
 }
 
 
 class EpisodesViewController: UITableViewController {
-    let episodes = [Episode(title: "Episode One"), Episode(title: "Episode Two"), Episode(title: "Episode Three")]
+	let episodes = [Episode(title: "Episode One"), Episode(title: "Episode Two"), Episode(title: "Episode Three")]
 
 	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return episodes.count
-    }
+		return episodes.count
+	}
 
 	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
 		let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
-        let episode = episodes[indexPath.row]
-        cell.textLabel?.text = episode.title
-        return cell
-    }
+		let episode = episodes[indexPath.row]
+		cell.textLabel?.text = episode.title
+		return cell
+	}
 }
 
 
 class DetailViewController: UIViewController {
-    @IBOutlet weak var label: UILabel? {
-        didSet {
-            label?.text = episode?.title
-        }
-    }
-    var episode: Episode?
+	@IBOutlet weak var label: UILabel? {
+		didSet {
+			label?.text = episode?.title
+		}
+	}
+	var episode: Episode?
 }
 
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-    var window: UIWindow?
+	var window: UIWindow?
 
-	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey : Any]? = nil) -> Bool {
-        return true
-    }
+	func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]? = nil) -> Bool {
+		return true
+	}
 }
 
